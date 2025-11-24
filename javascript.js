499e3403f8c0a374f28103e8be6b6c7741e28526
Darion <166887207+dariont5@users.noreply.github.com.>
2025-11-24 15:38:17 -0600

    build: add dialog for adding books
    html for dialog form was written. New book button
    opens dialog, cancel closes while saving input,
    submit closes, saves input, clears input.

--- javascript.js
+++ javascript.js
@@ -28,13 +28,37 @@ function addBookToLibrary(title, author, read) {
     myLibrary.push(book);
 }
 
+// render all books in library
 function renderBooks() {
     visibleLibrary.innerHTML = '';
 
     myLibrary.forEach((book) => {
         let visibleBook = document.createElement('li');
+        visibleBook.classList.add('book');
         visibleBook.textContent = book.title;
         visibleBook.id = book.id;
         visibleLibrary.appendChild(visibleBook);
     });
 }
+
+// add books button opens dialog form
+const newBookButton = document.querySelector('#new-book');
+const newBookDialog = document.querySelector('dialog');
+newBookButton.addEventListener('click', () => {
+    newBookDialog.showModal();
+    newBookDialog.focus();
+});
+
+// submit button adds book
+const cancelBook = document.querySelector('#cancel');
+const submitBook = document.querySelector('#submit-button');
+const newBookForm = document.querySelector('form');
+submitBook.addEventListener('click', (e) => {
+    e.preventDefault();
+    const data = new FormData(newBookForm);
+    addBookToLibrary(data.get('title'), data.get('author'), data.get('read'));
+    renderBooks()
+    newBookDialog.close();
+    newBookForm.reset();
+})
+
